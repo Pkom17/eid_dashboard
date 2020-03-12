e09ce5b7200f3673c513fed63ee1d15aeeae1d7e
Pkom17 <pkom17@gmail.com>
2020-03-12 06:00:26 +0000

    Update  sql

--- base/partner.sql
+++ base/partner.sql
@@ -1,6 +1,6 @@
-insert into partner values(1,'ACONDA VS',1);
-insert into partner values(2,'SEV-CI',1);
-insert into partner values(3,'EGPAF',1);
-insert into partner values(4,'ARIEL',1);
-insert into partner values(5,'HAI',1);
-insert into partner values(6,'ICAP',1);
+insert into partner(id, partner(id,name,active) values(1,'ACONDA VS',1);
+insert into partner(id, partner(id,name,active) values(2,'SEV-CI',1);
+insert into partner(id, partner(id,name,active) values(3,'EGPAF',1);
+insert into partner(id, partner(id,name,active) values(4,'ARIEL',1);
+insert into partner(id, partner(id,name,active) values(5,'HAI',1);
+insert into partner(id, partner(id,name,active) values(6,'ICAP',1);
